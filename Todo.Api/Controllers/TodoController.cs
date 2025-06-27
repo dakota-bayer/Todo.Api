d8844227b3fc6f0a22211884724682a29b61959a
Dakota Bayer <dakota.bayer@gmail.com>
2025-06-27 15:27:54 -0500

    added more todo requests and tested

--- Todo.Api/Controllers/TodoController.cs
+++ Todo.Api/Controllers/TodoController.cs
@@ -9,16 +9,9 @@ namespace Todo.Api.Controllers;
 public class TodoController : ControllerBase
 {
     private readonly AppDbContext _context;
-    private List<Models.Todo> _todoList;
 
     public TodoController(AppDbContext appDbContext)
     {
-        _todoList = new List<Models.Todo>
-        {
-            new() { Id = 1, TaskName = "Clean the litter box" },
-            new() { Id = 2, TaskName = "Brush teeth" }
-        };
-
         _context = appDbContext;
     }
 
@@ -29,6 +22,18 @@ public class TodoController : ControllerBase
         var items = await _context.TodoItems.ToListAsync();
 
         return Ok(items);
+    }
+
+    [HttpGet("{id}")]
+    [ProducesResponseType(typeof(Models.Todo), StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    public async Task<IActionResult> GetAsync(int id)
+    {
+        var item = await _context.TodoItems.FindAsync(id);
+
+        if (item == null) return NotFound();
+
+        return Ok(item);
     }
 
     [HttpPost]
@@ -47,5 +52,34 @@ public class TodoController : ControllerBase
         await _context.SaveChangesAsync();
 
         return Ok(todo);
+    }
+
+    [HttpPut("{id}")]
+    [ProducesResponseType(StatusCodes.Status204NoContent)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    public async Task<IActionResult> UpdateStatusAsync(int id)
+    {
+        var item = await _context.TodoItems.FindAsync(id);
+
+        if (item == null) return NotFound();
+
+        item.IsCompleted = !item.IsCompleted;
+        _context.TodoItems.Update(item);
+        await _context.SaveChangesAsync();
+        return NoContent();
+    }
+
+    [HttpDelete("{id}")]
+    [ProducesResponseType(StatusCodes.Status204NoContent)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    public async Task<IActionResult> DeleteAsync(int id)
+    {
+        var item = await _context.TodoItems.FindAsync(id);
+
+        if (item == null) return NotFound();
+
+        _context.TodoItems.Remove(item);
+        await _context.SaveChangesAsync();
+        return NoContent();
     }
 }
